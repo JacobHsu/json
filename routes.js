a70dfc870a07ee6762ae0efdf8596018a53aa56e
jacobhsu <jacob.hsu.tw@gmail.com>
2022-09-15 16:11:07 +0800

    feat: Collections NFT

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const goodsController = require('./controllers/goodsController');
+const collectionsController = require('./controllers/collectionsController');
 
 router.get('/', (req, res) => {
     res.send('Server is running')
 })
 
 router.use('/goods', goodsController);
+router.use('/collections', collectionsController);
 
 module.exports = router;
