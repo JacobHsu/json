f52e0c5f0c9313af8780beb988b76c501b5e7c67
jacobhsu <jacob.hsu.tw@gmail.com>
2022-11-23 13:01:07 +0800

    app use azure

--- config/express.js
+++ config/express.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 module.exports = (app) => {
     app.use(cors({origin: 'https://yellow-beach-0d1711d10.2.azurestaticapps.net', credentials: true}));
-    app.use(cors({origin: 'http://localhost:3000', credentials: true}));
+    //app.use(cors({origin: 'http://localhost:3000', credentials: true}));
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ limit: '50mb', extended: true }));
     app.use(cookieParser());
