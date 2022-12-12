4c569b912569ebc0dc461c02f10d9ec0c07a4a79
jacobhsu <jacob.hsu.tw@gmail.com>
2022-12-12 15:10:04 +0800

    fix: app.use originArr

--- config/express.js
+++ config/express.js
@@ -2,9 +2,11 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 // const auth = require('../middlewares/auth');
 const cors = require('cors');
+
+const originArr = ['http://localhost:3000', 'https://yellow-beach-0d1711d10.2.azurestaticapps.net']
 module.exports = (app) => {
-    app.use(cors({origin: 'https://yellow-beach-0d1711d10.2.azurestaticapps.net', credentials: true}));
-    //app.use(cors({origin: 'http://localhost:3000', credentials: true}));
+    //app.use(cors({origin: 'https://yellow-beach-0d1711d10.2.azurestaticapps.net', credentials: true}));
+    app.use(cors({origin: originArr, credentials: true}));
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ limit: '50mb', extended: true }));
     app.use(cookieParser());
