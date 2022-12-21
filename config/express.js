c27526f38a1852372609777bfd253c622a733f6a
jacobhsu <jacob.hsu.tw@gmail.com>
2022-12-21 10:19:03 +0800

    add demo site

--- config/express.js
+++ config/express.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 // const auth = require('../middlewares/auth');
 const cors = require('cors');
 
-const originArr = ['http://localhost:3000', 'https://yellow-beach-0d1711d10.2.azurestaticapps.net']
+const originArr = ['http://localhost:3000', 'https://yellow-beach-0d1711d10.2.azurestaticapps.net', 'https://purple-stone-06939cf10.2.azurestaticapps.net']
 module.exports = (app) => {
     //app.use(cors({origin: 'https://yellow-beach-0d1711d10.2.azurestaticapps.net', credentials: true}));
     app.use(cors({origin: originArr, credentials: true}));
